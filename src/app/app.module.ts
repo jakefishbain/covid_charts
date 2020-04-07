ef586d74d5f143304765d2acaa50a2a5e6edae9b
Jake Fishbain <jakefishbain@gmail.com>
2020-04-07 09:12:17 -0500

    mdbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
 
 import { StateBarGraph } from './components/state_bar_graph/state_bar_graph.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    StateBarGraph
+    StateBarGraph,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgxChartsModule
-
+    NgxChartsModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
