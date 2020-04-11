66fc5239221586fd0d1b6f4264dfc29cd1356405
Jake Fishbain <jakefishbain@gmail.com>
2020-04-11 18:05:51 -0500

    reorder table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -12,12 +12,12 @@ export class TableComponent implements OnInit {
     { 'header': 'Total Positives', 'key': 'positive' },
     { 'header': 'Total Negatives', 'key': 'negative' },
     { 'header': 'Total Deaths', 'key': 'death' },
+    { 'header': 'Currently Hospitalized', 'key': 'hospitalizedCurrently' },
+    { 'header': 'Currently in ICU', 'key': 'inIcuCurrently' },
     { 'header': 'Daily Tests', 'key': 'totalTestResultsIncrease' },
     { 'header': 'Daily Positive', 'key': 'positiveIncrease' },
     { 'header': 'Daily Negative', 'key': 'negativeIncrease' },
     { 'header': 'Daily Deaths', 'key': 'deathIncrease' },
-    { 'header': 'Currently Hospitalized', 'key': 'hospitalizedCurrently' },
-    { 'header': 'Currently in ICU', 'key': 'inIcuCurrently' },
   ]
 
   constructor() { }
