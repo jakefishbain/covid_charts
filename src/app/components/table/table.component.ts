d4cb4937c87c86991ddec6c720026a4347d8559d
Jake Fishbain <jakefishbain@gmail.com>
2020-04-11 17:33:31 -0500

    reverse trable data

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -16,17 +16,8 @@ export class TableComponent implements OnInit {
     { 'header': 'Currently in ICU', 'key': 'inIcuCurrently'},
   ]
 
-  constructor() {
-   }
+  constructor() {   }
 
-  ngOnInit(): void {  }
-
-  formatDay = (day) => {
-    let yyyy = day.slice(0,4)
-    let mm = day.slice(4,6)
-    let dd = day.slice(6,8)
-
-    return `${mm}/${dd}/${yyyy}`
-  }
+  ngOnInit(): void {   }
 
 }
